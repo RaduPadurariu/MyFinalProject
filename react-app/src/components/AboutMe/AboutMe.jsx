09be3949b399bb7d8d67cc436928a2bffb485605
Radu <padurariuionutradu@gmail.comgit config --global user.name Radugit config --global user.email padurariuionutradu@gmail.com>
2022-01-18 20:37:36 +0200

    Finish validations on Contact page

--- react-app/src/components/AboutMe/AboutMe.jsx
+++ react-app/src/components/AboutMe/AboutMe.jsx
@@ -8,49 +8,56 @@ import IconGithub from "../Icons/github.svg";
 export const AboutMe = () => {
     return (
         <div className="aboutMe">
+
             <div className="aboutMe-header">
                 About <span className="my1">Me</span>
             </div>
             <div className="aboutMe-container">
                 <div className="aboutMe-description">
                     <div className="aboutMe-description--title">
-                        <h3>I'm Radu and <span className="aboutMe-description--title-coding">I love Coding</span></h3>
+                        <h3> I'm Radu and <span className="aboutMe-description--title-coding">I love Coding</span></h3>
                     </div>
                     <div className="aboutMe-description--paragraph">
-                        <p>I'm and enthusiastic and detail-oriented junior Front end Developer. I am a person fascinated by technology, future, AI and space. My passion for informatics came from childhood when I decided to go to Informatics School. I like to create all kind of individual projects with React and Java Script. But the most I like to code and develop my skills every day with a team to share experience with others. My goal is to become a full time Front-End Web-Developer because I like to see immediate results of my work.</p>
+                        <p>I'm an enthusiastic and detail-oriented junior Front end Developer. I am a person fascinated by technology, future, AI and space. My passion for informatics came from childhood when I decided to go to Informatics School. I like to create all kind of individual projects with React and Java Script. But the most I like to code and develop my skills every day with a team to share experience with others. My goal is to become a full time Front-End Web-Developer because I like to see immediate results of my work.</p>
                     </div>
+                
                     <div className="aboutMe-description--downloadResume">
-                        <NavLink className= "aboutMe-description--downloadBtn" to="contact">
-                            Download Resume
-                                
-                        </NavLink>
+                        <a className= "aboutMe-description--downloadBtn" title="" target="_blank" href="/download/radupadurariuresume.pdf" download={"/download/radupadurariuresume.pdf"}>
+                            Download Resume    
+                        </a>
                     </div>
-                   
                 </div>
-
+               
                 <div className="aboutMe-carousel">
-                    <div>
-                        <button  className="aboutMe-carousel--Btn">{'<'}</button>
+                    <div className="aboutMe-carousel--title">
+                        Data Engineer memories ... 
                     </div>
-                    <div className="aboutMe-carousel--pics">
-                        <img className="aboutMe-carousel--pic1"src="../imgs/moon.jpg" alt="" />
-                    </div>
-                    <div>
-                        <button className="aboutMe-carousel--Btn">{'>'}</button>
+
+                    <div className="aboutMe-carousel--container">
+                        <div>
+                            <button  className="aboutMe-carousel--Btn">{'Back'}</button>
+                        </div>
+                        <div className="aboutMe-carousel--pics">
+                            <img className="aboutMe-carousel--pic1"src="../imgs/moon.jpg" alt="" />
+                        </div>
+                        <div>
+                            <button className="aboutMe-carousel--Btn">{'Next'}</button>
+                        </div>
                     </div>
 
                 </div>
             </div>
+
             <div className="aboutMe-social">
-                <NavLink to={{pathname:"https://developer.mozilla.org/en-US/docs/Glossary/HTML5"}} target="_blank">
+                <NavLink to={{pathname:"https://www.facebook.com/padugeolog"}} target="_blank">
                     <img className="icons" height={50} src={IconFacebook} alt="logo" />             
                 </NavLink>
 
-                <NavLink to={{pathname:"https://www.w3schools.com/css/"}} target="_blank">
+                <NavLink to={{pathname:"https://linkedin.com/in/ionut-radu-padurariu-13688991"}} target="_blank">
                     <img className="icons" height={50} src={IconLinkedin} alt="logo" />             
                 </NavLink>
                
-                <NavLink to={{pathname:"https://developer.mozilla.org/en-US/docs/Web/JavaScript"}} target="_blank">
+                <NavLink to={{pathname:"https://github.com/RaduPadurariu"}} target="_blank">
                     <img className="icons" height={50} src={IconGithub} alt="logo" />             
                 </NavLink>
             </div>
--- react-app/src/components/Contact/Contact.jsx
+++ react-app/src/components/Contact/Contact.jsx
@@ -1,6 +1,36 @@
 import "./Contact.css"
+import {useEffect, useState} from "react";
+
 
 export const Contact = () => {
+   
+    const [nameValue, setNameValue] = useState(false);
+    const [emailValue, setEmailValue] = useState(false);
+    const [messageValue, setMessageValue] = useState(false);
+    const [check, setCheck] = useState("");
+
+    const nameChange = (event) => {
+        setNameValue(event.target.value);
+        setCheck("");
+    }
+
+    const emailChange = (event) => {
+        setEmailValue(event.target.value);
+        setCheck("");
+    }
+
+    const messageChange = (event) => {
+        setMessageValue(event.target.value);
+        setCheck("");
+    }
+    const validation = () => {
+        if((nameValue !=="" && nameValue !== false) && (emailValue !=="" && emailValue !==false) && (messageValue !=="" && messageValue !==false)) {
+            setCheck(true);
+        }
+        else setCheck(false);
+
+    }  
+
     return (
         <div className="contact">
             <div className="contact-header">Contact <span className="me"> Me</span></div>
@@ -11,12 +41,36 @@ export const Contact = () => {
                     <label className="label-message"  htmlFor="message"> Message -</label>
                 </div>
                 <div className="contact--input-container">
-                    <input id="name" type="text" placeholder="type name"/>
-                    <input id="email"   type="text" placeholder="type email"/>
+                    <input  id="name" type="text" placeholder="type name" onChange={nameChange}/>
+                    <div className="contact--validation-container">
+                        <div className={`contact--validation ${nameValue==="" ? "" : " contact--validation-none"}`} >Please enter your name</div>
+                    </div>
+                    
+                    <input id="email"   type="text" placeholder="type email" onChange={emailChange}/>
+                    <div className="contact--validation-container">
+                        <div className={`contact--validation ${emailValue==="" ? "" : " contact--validation-none"}`}>Please enter a valid email</div>
+                    </div>
+                    
+
                     <div className="contact--btn-container">
-                        <textarea id="message"  className="contact--text-container"  type="text-area" placeholder="type message"/> 
+                        <div>
+                            <textarea id="message"  className="contact--text-container"  type="text-area" placeholder="type message" onChange={messageChange}/> 
+                            <div className="contact--validation-container">
+                                <div className={`contact--validation ${messageValue==="" ? "" : " contact--validation-none"}`}>Please enter some message</div>
+                            </div>
+                            <div className="contact--validation-container">
+                                <div className={`contact--validation-total ${check===true ? "" : " contact--validation-send"}`}> 
+                                    Your message has been send
+                                </div>
+                                <div className={`contact--validation ${check=== false ? "" : " contact--validation-none"}`}>
+                                        Some input fields are missing.  
+                                </div>
+                            </div>
+                            
+                        </div>
+                        
                         <div >
-                            <button className="contact--btn">Send</button>
+                            <button onClick={validation} className="contact--btn" >Send</button>
                         </div> 
                     </div>
                     
